f7677b130643fa9c249815c173bd40699b407972
Brian Sahota <bpsahota@gmail.com>
2020-08-31 21:18:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,5 +23,23 @@ def output_array(array)
   end
 end
 
+
+def return_string_array(array)
+  count = 0 
+  new_array = [ ]
+  while count < array.length do 
+    new_array.push(array[count]to_s)
+    count += 1 
+  end
+end
+
+  
+  
+  
+  
+  
+  
+  
+  
   
   
