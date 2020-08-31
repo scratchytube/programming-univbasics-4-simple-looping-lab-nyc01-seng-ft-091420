2305a6dfcb0d3746187130d007e0adadac2fd3ac
Brian Sahota <bpsahota@gmail.com>
2020-08-31 21:00:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,6 +8,13 @@ def loop_message_five_times(message)
 end
 
 def loop_message_n_times(message, limit)
-  
+  count = 0 
+  while count < limit do 
+    puts message 
+    count +=1 
+  end
+end
+
+def output_array(Array)
   
   
